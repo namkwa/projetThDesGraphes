d6fb4639a213f9636dcdb43c3dad941f835a3351
@Jean-Franck <jeanfranck.tang@gmail.com>
2020-11-14 14:57:34 +0100

    Graphe de tests

--- src/projetthdesgraphes/M1_Main.java
+++ src/projetthdesgraphes/M1_Main.java
@@ -7,12 +7,15 @@ import java.util.Scanner;
 public class M1_Main {
     public static void main(String[] args) throws FileNotFoundException {
         //On donne le chemin du fichier
-        File text = new File("./GraphesTests.txt");
+        File text = new File("./Graphe4");
 
         //On créer un objet Scanner avec le fichier en paramétre
         Scanner scan = new Scanner(text);
         M1_Graphes graphe = new M1_Graphes(scan);
+        System.out.println("Graphe original");
         System.out.println(graphe);
+        System.out.println();
+        System.out.println("Floyd Warshall");
         graphe.floyd_Warshall();
 
         }
